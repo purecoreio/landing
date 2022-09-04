4d17afdaa90371bc4c73719f51f005fd30a9e357
quiquelhappy <quiquelhappy@gmail.com>
2022-09-04 17:00:57 +0200

    use public images only

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		vue({
+		vue(/*{
 			template: {
 				transformAssetUrls: {
 					tags: {
@@ -15,7 +15,7 @@ export default defineConfig({
 					}
 				}
 			}
-		}),
+		}*/),
 		vuetify({ autoImport: true }),
 	],
 })
