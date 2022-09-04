409da8b1327a714aebd7bc239244f7b44976b695
quiquelhappy <quiquelhappy@gmail.com>
2022-09-04 17:05:23 +0200

    size fix

--- src/components/toolbar/Logo.vue
+++ src/components/toolbar/Logo.vue
@@ -3,7 +3,7 @@
         <!--<v-img alt="purecore.io logo" class="mr-3" contain src="/isotype.png" width="35" />-->
         <span class="mr-3" style="font-size: 30px; padding-bottom: 5px">
             purecore
-            <v-chip color="primary" small> BETA </v-chip>
+            <v-chip color="primary" size="small"> BETA </v-chip>
         </span>
     </div>
 </template>
