0e12a1fd1f1331ec2604c3c1cc46f6b5be5b24c0
quiquelhappy <quiquelhappy@gmail.com>
2024-03-04 19:40:21 +0100

    removed previous

--- src/components/toolbar/Logo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="d-flex align-center justify-center">
-        <!--<v-img alt="purecore.io logo" class="mr-3" contain src="/isotype.png" width="35" />-->
-        <span class="mr-3" style="font-size: 30px; padding-bottom: 5px">
-            purecore
-            <v-chip v-if="!$vuetify.display.mobile" color="primary" size="small"> BETA </v-chip>
-        </span>
-    </div>
-</template>
